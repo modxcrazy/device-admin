65b39082e549497f487a5c227fa3e520ac45b904
modxcrazy <in99soft.dev.pvt.ltd@gmail.com>
2025-05-16 21:23:28 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,12 @@
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyB306yl-qAYoSYVHP3LAby-aNiftH9OkpI",
+    authDomain: "devicead-992ca.firebaseapp.com",
+    projectId: "devicead-992ca",
+    storageBucket: "devicead-992ca.firebasestorage.app",
+    messagingSenderId: "160209495227",
+    appId: "1:160209495227:web:1d68b034fedddffd43759e",
+    measurementId: "G-CSCNLM5PL2"
 };
 
 // Initialize Firebase
